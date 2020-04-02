134b6fff16c595aaa5d7a7ce22badac870688eab
Mariano Matias Pereyra <marianopereyra95@gmail.com>
2020-04-02 11:10:12 -0300

    ommit inicial
    C

---
+++ scripts.js
@@ -0,0 +1,47 @@
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+      document.getElementById('clap').play();
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 83) {
+      document.getElementById('hihat').play();
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 68) {
+      document.getElementById('kick').play();
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 70) {
+      document.getElementById('openhat').play();
+      
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 71) {
+      document.getElementById('boom').play();
+      document.getElementById('BOOM2').classList = "selected, tecla" ;
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 72) {
+      document.getElementById('ride').play();
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 74) {
+      document.getElementById('snare').play();
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 75) {
+      document.getElementById('tom').play();
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 76) {
+      document.getElementById('tink').play();
+    }
+  });
